8fc281cac0fa325793c89c2eccf3aa0ebf70265a
Zach Green <greenzary@gmail.com>
2024-01-07 15:37:08 -0600

    wsuccessfully implemented openAI api

--- cli.js
+++ cli.js
@@ -21,21 +21,25 @@ async function interactWithChatGPT(prompt) {
 
   try {
     const result = await client.post(OPENAI_API_URL, params);
-    console.log("ChatGPT: ", result.data.choices[0].text.trim());
+    console.log("ChatGPT:", result.data.choices[0].text.trim());
+
   } catch (error) {
+
     if (error.response && error.response.status === 429) {
       console.warn("Rate limited. Retrying after a delay...");
       // Implement a backoff strategy, e.g., exponential backoff
       await new Promise((resolve) => setTimeout(resolve, 5000)); // Retry after 5 seconds
       await interactWithChatGPT(prompt); // Retry the request
     } else console.error("Error interacting with ChatGPT:", error.message);
+
   }
 }
 
 async function startChatGPTCLI() {
-  const inquirer = (await import("inquirer")).default;
 
-  console.log("Welcome to ChatGPT CLI!");
+  console.log("Welcome to ChatGPT CLI! \
+  \nEnter a prompt to begin chatting. \
+  \nTo exit the GPT CLI just type 'exit' at any time.");
 
   while (true) {
     const userPrompt = await askQuestion();
@@ -43,14 +47,12 @@ async function startChatGPTCLI() {
       console.log("Exiting ChatGPT CLI. Goodbye!");
       break;
     }
-
     await interactWithChatGPT(userPrompt);
   }
 }
 
 async function askQuestion() {
   const inquirer = (await import("inquirer")).default;
-
   const { answer } = await inquirer.prompt([
     {
       type: "input",
@@ -58,6 +60,7 @@ async function askQuestion() {
       message: "Enter your custom prompt: ",
     },
   ]);
+
   return answer;
 }
 
